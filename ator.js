77e5fdf798babecb671b7ca60d58c0c1c126dfae
Marloncostasouza <marloncostasouza@gmail.com>
2022-03-07 21:07:06 -0300

    first commit

---
+++ ator.js
@@ -0,0 +1,69 @@
+//Ator
+let yAtor = 366;
+let xAtor = 85;
+let colisao = false;
+let meusPontos = 0;
+
+function mostraAtor() {
+  image(imagemDoAtor, xAtor, yAtor, 30, 30);
+}
+function movimentaAtor() {
+  if (keyIsDown(UP_ARROW)) {
+    yAtor -= 3;
+  }
+  if (keyIsDown(DOWN_ARROW)) {
+    if (podeSeMover()) {
+      yAtor += 3;
+    }
+  }
+  if (keyIsDown(LEFT_ARROW)) {
+    xAtor -= 3;
+  }
+  if (keyIsDown(RIGHT_ARROW)) {
+    xAtor += 3;
+  }
+}
+
+function verificaColissao() {
+  //collideRectCircle(x1, y1, width1, height1, cx, cy, diameter)
+  for (let i = 0; i < imagemCarros.length; i++) {
+    colisao = collideRectCircle(
+      xCarros[i],
+      yCarros[i],
+      comprimentoCarro,
+      alturaCarro,
+      xAtor,
+      yAtor,
+      15
+    );
+    if (colisao) {
+      somDaColisao.play();
+      voltaAtorParaPosicaoinicial();
+      if (meusPontos > 0) {
+        meusPontos -= 1;
+      }
+    }
+  }
+}
+function voltaAtorParaPosicaoinicial() {
+  yAtor = 366;
+}
+
+function incluiPontos() {
+  textAlign(CENTER);
+  textSize(25);
+  fill(color(255, 240, 60));
+  text(meusPontos, width / 5, 27);
+}
+
+function marcaPonto() {
+  if (yAtor < 15) {
+    meusPontos += 1;
+    somDoPonto.play();
+    voltaAtorParaPosicaoinicial();
+  }
+}
+
+function podeSeMover() {
+  return yAtor < 366;
+}
